94c2263a269799a4917be88069af6220654873df
BryanPlazaP <bplazap@est.ups.edu.ec>
2024-07-19 11:37:03 -0500

    Add(I): Adding Get-Animal-By-Id Method Using Repository Data

--- src/main/java/com/ups/oop/service/AnimalService.java
+++ src/main/java/com/ups/oop/service/AnimalService.java
@@ -5,12 +5,14 @@ import com.ups.oop.dto.Person;
 import com.ups.oop.dto.PersonDTO;
 import com.ups.oop.entity.Animal;
 import com.ups.oop.repository.AnimalRepository;
+import jakarta.persistence.Id;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class AnimalService {
@@ -23,8 +25,8 @@ public class AnimalService {
 
     public ResponseEntity createAnimal(AnimalDTO animal) {
         String animalId = animal.getId();
-        boolean wasFound = findAnimal (animalId);
-        if(wasFound){
+        boolean wasFound = findAnimal(animalId);
+        if (wasFound) {
             String errorMessage = "Animal with id " + animalId + " already exists";
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                     .body("Animal with id " + animal.getId() + " already exists");
@@ -34,8 +36,8 @@ public class AnimalService {
         }
     }
 
-    private boolean findAnimal(String id){
-        for(AnimalDTO animal: AnimalList) {
+    private boolean findAnimal(String id) {
+        for (AnimalDTO animal : AnimalList) {
             if (id.equalsIgnoreCase(animal.getId())) {
                 return true;
             }
@@ -45,27 +47,40 @@ public class AnimalService {
 
 
     public ResponseEntity getAllAnimal() {
-        Iterable <Animal> animalIterable = animalRepository.findAll();
-        List<AnimalDTO> animalList = new ArrayList<>();
-
-        for(Animal a: animalIterable){
-            AnimalDTO animal = new AnimalDTO(a.getId().toString(), a.getLenght(), a.getHeight(), a.getWeight(), a.getColor(), a.getBreed(), a.getName());
-        animalList.add(animal);
+        Iterable<Animal> animalIterable = animalRepository.findAll();
+        List<AnimalDTO> animals = new ArrayList<>();
+        for (Animal a : animalIterable) {
+            AnimalDTO animal = new AnimalDTO();
+            animal.setAnimalCode(a.getName() + "-" + a.getBreed() + "-" + a.getColor());
+            animal.setWeight(a.getWeight());
+            animal.setLenght(a.getLenght());
+            animal.setHeight(a.getHeight());
+            animals.add(animal);
         }
-        if(animalList.isEmpty()) {
+        if (animals.isEmpty()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Animal List Not found");
         }
-        return ResponseEntity.status(HttpStatus.OK).body(animalList);
+        return ResponseEntity.status(HttpStatus.OK).body(animals);
     }
+
     public ResponseEntity getAnimalById(String id) {
-        for(AnimalDTO ani : AnimalList){
-            if(id.equalsIgnoreCase(ani.getId())){
-                return ResponseEntity.status(HttpStatus.OK).body(ani);
-            }
+        Optional<Animal> animalOptional = animalRepository.findById(Long.valueOf(id));
+        //Optional<Animal> animalOptional = animalRepository.findById(animalid);
+        if (animalOptional.isPresent()) {
+            //if record was found
+            Animal animalFound = animalOptional.get();
+            AnimalDTO animal = new AnimalDTO(animalFound.getId().toString(),
+                    animalFound.getName() + "-" + animalFound.getBreed() + "-" + animalFound.getColor(),
+                    animalFound.getWeight(), animalFound.getLenght(), animalFound.getHeight());
+            return ResponseEntity.status(HttpStatus.OK).body(animal);
+        } else {
+            //if record wasn't found
+            String errorMessage = "animal with id " + id + " not found";
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorMessage);
         }
-        String errorMessage = "animal with id " + id + " not found";
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorMessage);
     }
+
+
 
     public AnimalDTO updateAnimal(String id, AnimalDTO animal){
         AnimalDTO ani = new AnimalDTO();
